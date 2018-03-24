705c6348577cc591c9ef7487214135f64e1414bc
Simon Holloway <simon.holloway@givesmart.com>
2018-03-24 15:03:34 +0000

    implement an interface

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type greeter interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	printGreeting(eb)
+
+	sb := spanishBot{}
+	printGreeting(sb)
+}
+
+func (englishBot) getGreeting() string {
+	return "Hello"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola"
+}
+
+func printGreeting(b greeter) {
+	fmt.Println(b.getGreeting())
 }
